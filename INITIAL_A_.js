f968f9ef2f314b72ca712bc3b6c0523d34c49a0b
fergarundel <68985154+fergarundel@users.noreply.github.com>
2020-07-30 22:28:06 +1000

    Update INITIAL_A_.js

--- INITIAL_A_.js
+++ INITIAL_A_.js
@@ -7,7 +7,7 @@ background (255,255,255);
 
 function draw() {
 background (255,255,255);
-strokeWeight(mouseX/5);
+strokeWeight((mouseX+mouseY)/14);
 stroke (0,0,255);
 line (115,400,215,100);
 line (215,100,285,100);
