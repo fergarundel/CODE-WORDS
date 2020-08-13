54e9ab69062ce97ed1111618737199c57d7649e6
fergarundel <68985154+fergarundel@users.noreply.github.com>
2020-08-13 15:21:44 +1000

    Add files via upload

---
+++ Week_4/spinning_words/spinning_words.js
@@ -0,0 +1,30 @@
+//fergus arundel
+
+function preload () {
+  NeueMetana = loadFont ('data/NeueMetana-Regular.otf'); 
+}
+
+var spin = 0;
+
+function setup() {
+  createCanvas (600,600);
+  background (255);
+  fill (0,0,255);
+  angleMode (DEGREES);
+  textSize (32);
+  textFont (NeueMetana);
+  textAlign (CENTER);
+
+}
+
+function draw() {
+  background (255,30);
+  translate (300,300);
+  fill (0,0,255);
+  rotate(spin);
+  textSize (50);
+  text ('hello world',0,0);
+  
+  spin = spin + map(mouseX,0,600,-5,5); // map uses mouses value to change direction of rotation
+}
+  
