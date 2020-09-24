4a8623842abd96db6e7841f3c7b291f0b0c7f7c9
fergarundel <68985154+fergarundel@users.noreply.github.com>
2020-09-24 14:57:02 +1000

    Add files via upload

---
+++ week_09/text_points/text_points.js
@@ -0,0 +1,36 @@
+let Metana;
+let cell = [];
+
+function preload() {
+   Metana = loadFont ('data/NeueMetana-Regular.otf');
+}
+
+function setup() {
+  createCanvas (windowWidth,windowHeight);
+  cell = new Cell('FREE INTERNATIONAL UNIVERSITY');
+}
+
+function draw() {
+  background(0,150,100);
+  cell.drawCells(60);
+}
+
+class Cell {
+  constructor (type){
+    this.type = type;
+  }
+  
+  drawCells(size){
+    var cells = Metana.textToPoints(this.type,100,height/2,size,{
+      sampleFactor: .3,
+    })
+    
+    for (var i = 0; i < cells.length; i++){
+      var cell = cells[i];
+      stroke (255);
+      strokeWeight (3);
+      point (cell.x+random(-1,1),cell.y+random(-1,1));  
+    }    
+  }
+  
+}
